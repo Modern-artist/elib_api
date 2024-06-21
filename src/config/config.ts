c5fcbc34537cae602de7811253116a9a5ceb52f6
Modern-artist <90668597+Modern-artist@users.noreply.github.com>
2024-06-21 14:35:59 +0530

    add error handling

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,6 +5,7 @@ conf();
 const _config = {
   port: process.env.PORT,
   dbUrl: process.env.MONGO_CONNECTION_STRING,
+  jwtSecret: process.env.jwtSecret,
 };
 
 export const config = Object.freeze(_config);
--- src/user/userControllers.ts
+++ src/user/userControllers.ts
@@ -2,33 +2,46 @@ import { NextFunction, Request, Response } from "express";
 import userModel from "./userModel";
 import createHttpError from "http-errors";
 import bcrypt from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { config } from "../config/config";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   // validation
-  console.log("request :", req.body);
+  //   console.log("request :", req.body);
   const { name, email, password } = req.body;
   // validation
 
   // Database se me search karna hai
-  const user = await userModel.findOne({ email });
-
-  if (user) {
-    const err = createHttpError(400, "user Already existed");
-    return next(err);
+  try {
+    const user = await userModel.findOne({ email });
+    if (user) {
+      const err = createHttpError(400, "user Already existed");
+      return next(err);
+    }
+    console.log("found user");
+  } catch (error) {
+    return next(createHttpError(500, "error in finding user"));
+    // console.log("error in finding user", error);
   }
 
-  const hashPassword = await bcrypt.hash(password, 10);
-  const newUser = await userModel.create({
-    name,
-    email,
-    password: hashPassword,
-  });
-
+  try {
+    const hashPassword = await bcrypt.hash(password, 10);
+    const newUser = await userModel.create({
+      name,
+      email,
+      password: hashPassword,
+    });
+    const token = sign({ sub: newUser._id }, config.jwtSecret as string, {
+      expiresIn: "7d",
+    });
+    // response
+    res.json({
+      "access token": token,
+    });
+  } catch (error) {
+    return next(createHttpError(500, "error in creating user"));
+  }
   // process
-  // response
-  res.json({
-    message: "created User",
-  });
 };
 
 export { createUser };
